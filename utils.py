dc31b9f3a3b0aacffaa30dcef8b4cff739ba12a6
Michael Gharbi <gharbi@mit.edu>
2017-12-15 13:35:09 -0500

    adds checkpointer

--- utils.py
+++ utils.py
@@ -27,3 +27,42 @@ def save(checkpoint, model, params, optimizer, step):
     'step': step,
     } , checkpoint)
 
+
+class Checkpointer(object):
+  def __init__(self, directory, model, optimizer, 
+               max_save=-1,
+               filename='ckpt.pth.tar', verbose=False):
+    if directory.startswith('~'):
+        directory = os.path.expanduser(directory)
+    self.model = model
+    self.optimizer = optimizer
+    self.max_save = max_save
+    self.directory = directory
+    self.filename = filename
+    self.verbose = verbose
+
+    self.old_files = []
+
+
+  def save_checkpoint(self, epoch, filename, is_best=False):
+    th.save({ 
+        'epoch': epoch + 1,
+        'state_dict': self.model.state_dict(),
+        'optimizer' : self.optimizer.state_dict(),
+        }, filename)
+    if is_best:
+      shutil.copyfile(filename, os.path.join(self.directory, 'best.pth.tar'))
+
+  def on_epoch_end(self, epoch, logs=None):
+    filename = '{:03d}'.format(epoch+1)
+    if self.verbose > 0:
+      print('\nEpoch %i: saving model to %s' % (epoch+1, file))
+    self.save_checkpoint(epoch, filename)
+    if self.max_save > 0:
+      if len(self.old_files) == self.max_save:
+        try:
+          os.remove(self.old_files[0])
+        except:
+          pass
+        self.old_files = self.old_files[1:]
+      self.old_files.append(filename)
