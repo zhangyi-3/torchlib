f93e721cbc0a04a757979c3572a6d87b2d7cb841
Michael Gharbi <gharbi@mit.edu>
2018-05-17 18:30:38 -0400

    adds env input to debug

--- debug.py
+++ debug.py
@@ -2,9 +2,9 @@ import torch as th
 import torchlib.viz as viz
 import numpy as np
 
-def tensor(t, key="none"):
+def tensor(t, key="none", env="debug"):
   t = t.detach()
-  v = viz.BatchVisualizer(key, env="debug")
+  v = viz.BatchVisualizer(key, env=env)
   bs, c, h, w = t.shape
   mu = t.mean()
   std = t.std()
@@ -12,20 +12,20 @@ def tensor(t, key="none"):
   t = th.clamp(0.5*(t+1), 0, 1)
   v.update(t.cpu().numpy(), caption="{} {:.2f} ({:.2f})".format(key, mu, std))
 
-def histogram(t, bins=10, key="none"):
-  v = viz.HistogramVisualizer(key, env="debug")
+def histogram(t, bins=10, key="none", env="debug"):
+  v = viz.HistogramVisualizer(key, env=env)
   v.update(t.detach().cpu().numpy(), numbins=bins)
 
-def scatter(x, y, key="none"):
-  v = viz.ScatterVisualizer(key, env="debug")
+def scatter(x, y, key="none", env="debug"):
+  v = viz.ScatterVisualizer(key, env=env)
   xx = th.cat([x.view(-1, 1), y.view(-1, 1)], 1).detach().cpu().numpy()
   v.update(xx)
 
-def line(x, y, key="none", ylog=False):
+def line(x, y, key="none", ylog=False, env="debug"):
   opts = {}
   if ylog:
     opts["ytype"] = "log"
-  v = viz.ScalarVisualizer(key, env="debug", opts=opts)
+  v = viz.ScalarVisualizer(key, env=env, opts=opts)
   xx = np.ravel(x)
   yy = np.ravel(y)
   for i in range(xx.size):
